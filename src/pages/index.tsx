f6ea62e9bbb5a6535c144c91392955dae35727a2
junyoung <jadw9715@gmail.com>
2023-04-11 22:52:04 +0900

    layout.  순서 정하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,23 @@ import { Box, Container } from "@mui/material";
 export default function Home() {
   return (
     <Container>
-      <Box sx={{ border: "1px solid", p: 2, mb: 3 }}>이미지</Box>
-      <Box sx={{ border: "1px solid", p: 2 }}></Box>
+      <Box sx={{ border: "1px solid", p: 2, mb: 3 }}>
+        메인 배너(이미지나 동영상)
+      </Box>
+      <Box sx={{ border: "1px solid", p: 2, mb: 3 }}>
+        소중한 분들을 초대합니다~ 블라블라(string)
+      </Box>
+      <Box sx={{ border: "1px solid", p: 2, mb: 3 }}>
+        웨딩 갤러리(loop slide gallery)
+      </Box>
+      <Box sx={{ border: "1px solid", p: 2, mb: 3 }}>달력 날짜</Box>
+      <Box sx={{ border: "1px solid", p: 2, mb: 3 }}>음식 안내</Box>
+      <Box sx={{ border: "1px solid", p: 2, mb: 3 }}>지도 (map API)</Box>
+      <Box sx={{ border: "1px solid", p: 2, mb: 3 }}>교통안내</Box>
+      <Box sx={{ border: "1px solid", p: 2, mb: 3 }}>방명록 남기기</Box>
+      <Box sx={{ border: "1px solid", p: 2, mb: 3 }}>
+        마음 전하실 곳 (계좌번호 복사 기능)
+      </Box>
     </Container>
   );
 }
