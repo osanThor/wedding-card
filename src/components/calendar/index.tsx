8a887fba158a270c3a8788a7a621b7a94e160386
junyoung <jadw9715@gmail.com>
2023-04-17 23:54:48 +0900

    feat. moment를 사용한 달력

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,6 +1,12 @@
+import GeneratorDate from "@/utils/generatorDate";
 import { Box } from "@mui/material";
+import moment from "moment";
+import { useState } from "react";
 
 const Calendar = () => {
+  const [date, setDate] = useState<moment.Moment>(() => moment("2024.01.27"));
+
+  GeneratorDate.generatorDate(date);
   return <Box>달력</Box>;
 };
 
